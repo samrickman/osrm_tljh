22adf0220173b6bfff35f4d3fc50259bf476862d
samrickman <46163241+samrickman@users.noreply.github.com>
2020-02-25 11:43:33 +0000

    Fix: replaced broken dependency

--- install_gdal_packages.sh
+++ install_gdal_packages.sh
@@ -8,3 +8,5 @@ export C_INCLUDE_PATH=/usr/include/gdal
 ogrinfo --version # get version - in this case 2.4.2
 sudo -Ei pip install numpy pandas sklearn spacy matplotlib seaborn GDAL==2.4.2 osrm
 
+# replace broken import in old version with current import
+sudo sed -i 's/\bfrom matplotlib.mlab import griddata\b/from scipy.interpolate import griddata/g' /opt/tljh/user/lib/python3.7/site-packages/osrm/extra.py
