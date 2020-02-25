e502ee1e4d73b0f5963b84518c349420dc233d6d
samrickman <46163241+samrickman@users.noreply.github.com>
2020-02-25 11:12:33 +0000

    Fix: update osrm dependency to correct package

--- install_gdal_packages.sh
+++ install_gdal_packages.sh
@@ -6,5 +6,5 @@ sudo apt-get -y install gdal-bin libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
 ogrinfo --version # get version - in this case 2.4.2
-sudo -Ei pip install numpy pandas sklearn spacy matplotlib seaborn GDAL==2.4.2 osrm-py
+sudo -Ei pip install numpy pandas sklearn spacy matplotlib seaborn GDAL==2.4.2 osrm
 
